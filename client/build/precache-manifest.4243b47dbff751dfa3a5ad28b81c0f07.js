7762d0d4ae5afa5e2f14af6d7f28c97e6dd485cb
Vic <t18042k@gmail.com>
2020-11-06 20:55:41 +0200

    build app

---
+++ client/build/precache-manifest.4243b47dbff751dfa3a5ad28b81c0f07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb919cd0a11fe47fb9213d92d896b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c027adf22ded82566d4",
+    "url": "/static/css/2.cbe6aa73.chunk.css"
+  },
+  {
+    "revision": "7f14051fbf50369349cf",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "4c027adf22ded82566d4",
+    "url": "/static/js/2.ca7d5743.chunk.js"
+  },
+  {
+    "revision": "e6ff11346de71d063c53089dedf57b03",
+    "url": "/static/js/2.ca7d5743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f14051fbf50369349cf",
+    "url": "/static/js/main.78067be4.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/static/media/victory.6db44b04.png"
+  }
+]);
